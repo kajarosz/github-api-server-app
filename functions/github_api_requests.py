c88a4ae2e7fa544fc922fb9e1a68f1dfd6f1a083
kajarosz <karolinaannajarosz@gmail.com>
2022-04-09 19:41:54 +0200

    Applied DRY rule to functions.

--- functions/github_api_requests.py
+++ functions/github_api_requests.py
@@ -1,31 +1,30 @@
 import requests
 
-def github_repos(user_name):
+def github_details(user_name):
     github_response = requests.get(f'https://api.github.com/users/{user_name}/repos').json()
     repos =  []
     for repo in github_response:
         name = repo.get('name')
         stars = repo.get('stargazers_count')
         repos.append({'name': name, 'stars': stars})
+    return repos
+
+def github_repos(user_name):
+    repos = github_details(user_name)
     return {f'{user_name}': repos}
 
 def github_stars(user_name):
-    github_response = requests.get(f'https://api.github.com/users/{user_name}/repos').json()
+    repos = github_details(user_name)
     user_stars =  0
-    for repo in github_response:
-        repo_stars = repo.get('stargazers_count')
-        user_stars = user_stars + repo_stars
+    for repo in repos:
+        user_stars = user_stars + repo.get('stars')
     return {'user_stars': user_stars}
 
 def github_languages(user_name):
-    github_response = requests.get(f'https://api.github.com/users/{user_name}/repos').json()
-    repos =  []
-    for repo in github_response:
-        name = repo.get('name')
-        repos.append(name)
+    repos = github_details(user_name)
     lang_dict = {}
     for repo in repos:
-        github_response = requests.get(f'https://api.github.com/repos/{user_name}/{repo}/languages').json()
+        github_response = requests.get('https://api.github.com/repos/{0}/{1}/languages'.format(user_name, repo.get('name'))).json()
         lang_dict = {k: lang_dict.get(k, 0) + github_response.get(k, 0) for k in set(lang_dict) | set(github_response)}
     lang_list = list(lang_dict.items())
     lang_list.sort(reverse=True, key = lambda x: x[1])
